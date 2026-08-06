c65c640bf21159c68641aa404fa57b1c58c87ad1
DevVictor1 <appvictorr@gmail.com>
2026-08-06 14:43:27 +0100

    Link auth branding to landing page

--- client/src/components/AuthBranding.jsx
+++ client/src/components/AuthBranding.jsx
@@ -1,8 +1,9 @@
+import { Link } from "react-router-dom";
 import estateHubIcon from "../assets/branding/estatehub-icon.png";
 
 function AuthBranding({ subtitle }) {
   return (
-    <div className="auth-branding">
+    <Link to="/" className="auth-branding" aria-label="Go to EstateHub home">
       <img
         src={estateHubIcon}
         alt="EstateHub icon"
@@ -12,7 +13,7 @@ function AuthBranding({ subtitle }) {
         <h1 className="auth-branding-title">EstateHub</h1>
         <p className="auth-branding-subtitle">{subtitle}</p>
       </div>
-    </div>
+    </Link>
   );
 }
 
